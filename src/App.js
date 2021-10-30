7588bc151f081655ff20bd8e297fb5e58d89e764
chamberezigbo <chamberezigbo@gmail.com>
2021-10-30 03:13:48 +0100

    update our manu-items to navigate withRouter hoc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import { Switch,Route } from 'react-router';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component = {HatsPage} />
+      </switch>
     </div>
   );
 }
